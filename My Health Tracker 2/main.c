2754582a28c80438ac1b09841fbe3ca5815a916a
Beatriz <beatrizachi95@gmail.com>
2025-11-22 16:46:52 +0800

    Initial Commit

---
+++ My Health Tracker 2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  My Health Tracker 2
+//
+//  Created by Beatriz Anak Anthony on 22/11/2025.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
